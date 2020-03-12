596918b045f5b03ee02ee50c967a3cc22f69fe02
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-03-12 19:53:29 +0900

    WeatherHttpClient added

--- app/src/main/java/com/gardenlab/androidkokoa/Fragment4.java
+++ app/src/main/java/com/gardenlab/androidkokoa/Fragment4.java
@@ -1,18 +1,28 @@
 package com.gardenlab.androidkokoa;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment4 extends Fragment {
+    final String TAG = "WEATHER";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment4,container,false);
+        View rootView = (View) inflater.inflate(R.layout.fragment4,container,false);
+
+        WeatherHttpClient httpClient = new WeatherHttpClient();
+        String data = httpClient.getWeatherData("Seoul");
+        Log.d(TAG, "Weather Data: "+data);
+
+        return rootView;
     }
 }
